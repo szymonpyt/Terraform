b9f6b6e2b53c2d0ccdfe2de09bd1441f51a70902
szymonpyt <szymon_21@wp.pl>
2023-03-07 19:54:04 +0100

    Clean up

--- auto_scaling_group/main.tf
+++ auto_scaling_group/main.tf
@@ -1,13 +1,8 @@
 resource "aws_launch_template" "terraform_launch_template" {
-  name = var.template_name
-
-  image_id = var.ami
-
-  instance_type = length(var.instance_types) == 0 ? "t2.micro" : null
-
-  key_name = var.key
-
+  name                   = var.template_name
+  image_id               = var.ami
+  instance_type          = length(var.instance_types) == 0 ? "t2.micro" : null
+  key_name               = var.key
   vpc_security_group_ids = [var.sg_id]
-
-
 }
+
