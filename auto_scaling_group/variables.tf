aeaadecbafde5a2ad4f0024497808c03546cdfc7
szymonpyt <szymon_21@wp.pl>
2023-03-07 19:51:43 +0100

    Added .gitignore

--- auto_scaling_group/variables.tf
+++ auto_scaling_group/variables.tf
@@ -1,9 +1,24 @@
 variable "template_name" {
   type    = string
-  default = "terraform template"
+  default = "terraform_template"
 }
 
 variable "ami" {
-  type = string
+  type    = string
   default = "ami-0568936c8d2b91c4e"
+}
+
+variable "instance_types" {
+   type    = list(any)
+   default = []
+}
+
+variable "key" {
+  type    = string
+  default = "docker-aws-s3"
+}
+
+variable "sg_id" {
+  type    = string
+  default = "sg-b119b1f9"
 }
